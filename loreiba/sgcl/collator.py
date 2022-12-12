60b4e82ca792a4d341ed0759863209e7b6bf5a01
Luke Gessler <lukegessler@gmail.com>
2022-12-12 16:54:42 -0500

    make collator accommodate spans

--- loreiba/sgcl/collator.py
+++ loreiba/sgcl/collator.py
@@ -9,31 +9,43 @@ from transformers import DataCollatorForLanguageModeling
 
 @DataCollator.register("loreiba.sgcl.collator::collator")
 class SgclDataCollator(DataCollator):
-    def __init__(self, tokenizer: Lazy[Tokenizer], text_field: str = "input_ids"):
+    def __init__(self, tokenizer: Lazy[Tokenizer], text_field: str = "input_ids", span_field: str = "token_spans"):
         tokenizer = tokenizer.construct()
         self.tokenizer = tokenizer
         self.text_pad_id = tokenizer.pad_token_id
         self.text_field = text_field
+        self.span_field = span_field
         self.mlm_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
 
     def __call__(self, batch) -> Dict[str, Any]:
         padded = {}
         for key in batch[0].keys():
             max_length = max(len(item[key]) for item in batch)
-            pad_id = 0 if key != self.text_field else self.text_pad_id
-            value = torch.vstack(
-                [
+            pad = [self.text_pad_id] if key == self.text_field else [[-1, -1]] if key == self.span_field else [0]
+            if key == self.span_field:
+                seq_values = [
+                    torch.vstack(
+                        (
+                            item[key],
+                            torch.tensor(pad * (max_length - len(item[key])), dtype=torch.long).to(item[key].device),
+                        ),
+                    )
+                    if (max_length - len(item[key])) > 0
+                    else item[key]
+                    for item in batch
+                ]
+                value = torch.stack(seq_values, dim=0).to(batch[0][key].device)
+            else:
+                seq_values = [
                     torch.hstack(
                         (
                             item[key],
-                            torch.tensor([pad_id] * (max_length - len(item[key])), dtype=torch.long).to(
-                                item[key].device
-                            ),
+                            torch.tensor(pad * (max_length - len(item[key])), dtype=torch.long).to(item[key].device),
                         )
                     )
                     for item in batch
                 ]
-            ).to(batch[0][key].device)
+                value = torch.vstack(seq_values).to(batch[0][key].device)
             if key == self.text_field:
                 input_ids, labels = self.mlm_collator.torch_mask_tokens(value)
                 num_masked = (input_ids.view(-1) == self.tokenizer.mask_token_id).sum().item()
