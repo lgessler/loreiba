9b965c04bb86d53dfad02fbc5644f037711fbf58
Luke Gessler <lukegessler@gmail.com>
2022-12-24 10:32:13 -0500

    oops

--- loreiba/data.py
+++ loreiba/data.py
@@ -83,7 +83,7 @@ class ReadTextOnlyConllu(Step):
 
         def read_conllu(path):
             with open(path, "r") as f:
-                sentences = [[t["form"] for t in s] for s in conllu.parse(f.read())][:256]
+                sentences = [[t["form"] for t in s] for s in conllu.parse(f.read())]
                 if stanza_retokenize:
                     sentences = retokenize(sentences, path)
                 return sentences
