38c301f4f65e3e6518898cf06e47a5495b2ae590
Luke Gessler <lg876@georgetown.edu>
2022-12-29 13:06:54 -0500

    only compute sgcl losses during training

--- loreiba/sgcl/model.py
+++ loreiba/sgcl/model.py
@@ -111,13 +111,13 @@ class SGCLModel(Model):
             mlm_loss = self._mlm_loss(mlm_preds, labels)
             loss = mlm_loss
             outputs["mlm_loss"] = mlm_loss
-            if self.tree_sgcl_config is not None:
+            if self.training and self.tree_sgcl_config is not None:
                 tree_loss = syntax_tree_guided_loss(self.tree_sgcl_config, hidden_states, token_spans, tree_sets)
                 loss += tree_loss
                 print(f" tree_loss: {tree_loss:0.4f}", end="")
                 log_metric("tree", tree_loss)
                 outputs["tree_loss"] = tree_loss
-            if self.phrase_sgcl_config is not None:
+            if self.training and self.phrase_sgcl_config is not None:
                 phrase_loss = phrase_guided_loss(self.phrase_sgcl_config, attentions, attention_mask, phrase_sets)
                 loss += phrase_loss
                 print(f" phrase_loss: {phrase_loss:0.4f}", end="")
